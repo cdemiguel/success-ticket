e9cb891c161e59250beecfcd85eb9a5de02cae4f
cdemiguel <demiguelferrerocarlos@gmail.com>
2018-03-27 15:21:14 +0200

    upload test server-api and passport iteration

---
+++ success-ticket/client-api/src/index.js
@@ -0,0 +1,101 @@
+const axios = require("axios")
+
+const api_client = {
+  baseUrl() {
+    return `${this.protocol}://${this.host}:${this.port}`
+  },
+
+  loginUser(username, password) {
+    return axios
+      .post(`${this.baseUrl()}/events/login`, { username, password })
+      .then(res => res.data)
+  },
+
+  getCompanyName(userId) {
+    return axios
+      .get(`${this.baseUrl()}/events/company/${userId}`)
+      .then(res => res.data.data)
+  },
+
+  getCompanyIdByUser(userId) {
+    return axios
+      .get(`${this.baseUrl()}/events/companyid/${userId}`)
+      .then(res => res.data.data)
+  },
+
+  getEventListByCompanyId(companyId) {
+    return axios
+      .get(`${this.baseUrl()}/events/companyid/events/${companyId}`)
+      .then(res => res.data.data)
+  },
+
+  getUserById(userId) {
+    return axios
+      .get(`${this.baseUrl()}/events/user/${userId}`)
+      .then(res => res.data.data)
+  },
+
+  getEventList(userId) {
+    return axios
+      .get(`${this.baseUrl()}/events/${userId}`)
+      .then(res => res.data.data)
+  },
+
+  getSessionsList(eventId) {
+    return axios
+      .get(`${this.baseUrl()}/events/sessions/${eventId}`)
+      .then(res => res.data.data)
+  },
+
+  getTicketsList(eventId, sessionId) {
+    return axios
+      .get(`${this.baseUrl()}/events/sessions/tickets/${eventId}/${sessionId}`)
+      .then(res => res.data.data)
+  },
+
+  getTicket(eventId, sessionId, ticketId) {
+    return axios
+      .get(
+        `${this.baseUrl()}/events/sessions/ticket/${eventId}/${sessionId}/${ticketId}`
+      )
+      .then(res => res.data)
+  },
+
+  validateTicket(eventId, sessionId, ticketId) {
+    return axios
+      .put(
+        `${this.baseUrl()}/events/sessions/validate-ticket/${eventId}/${sessionId}/${ticketId}`
+      )
+      .then(res => res.data)
+  },
+
+  createUser(name, username, email, password, rol, companyId, eventsId) {
+    return axios
+      .post(`${this.baseUrl()}/events/usercreate`, {
+        name,
+        username,
+        email,
+        password,
+        rol,
+        companyId,
+        eventsId
+      })
+      .then(res => res.data)
+  },
+
+  getUsersByCompanyId(companyId) {
+    return axios
+      .get(`${this.baseUrl()}/events/user/company/${companyId}`)
+      .then(res => res.data.data)
+  },
+
+  deleteUser(email, role, emailUser) {
+    return axios
+      .delete(`${this.baseUrl()}/events/user/delete/${email}`, {
+        data: { role, emailUser }
+      })
+      .then(res => res.data)
+  }
+}
+
+module.exports = api_client
---
+++ success-ticket/client-api/tests/test.js
@@ -0,0 +1,113 @@
+require('dotenv').config();
+
+const api_client = require('../src/index')
+const assert = require('assert')
+const expect = require('chai').expect
+
+const { API_PROTOCOL, API_HOST, API_PORT } = process.env;
+
+api_client.protocol = API_PROTOCOL;
+api_client.host = API_HOST;
+api_client.port = API_PORT;
+
+describe('Testing API client', () => {
+
+    const email = 'mail@mail.com'
+	const password = '123'
+    const userID = '5ab987cc54f2aaf2e220ebd6'
+    const companyId = '5ab13447031c9c2fa3193b97'
+    const eventId = '5ab13447031c9c2fa3193b99'
+    const sessionId = '5ab13447031c9c2fa3193b9a'
+    const ticketId = '5ab13447031c9c2fa3193b9b'
+
+    const nameUser = 'Demiguel_test123456'
+    const usernameUser = 'username_test123456'
+    const emailUser = 'test@tes.com123456'
+    const passwordUser = '123456'
+    const rolAdmin = 'admin'
+    const rolUser = 'validator'
+    const eventsId = ['5ab13447031c9c2fa3193b99']
+    const companysId = ['5ab13447031c9c2fa3193b97']
+
+    it('should login an user', done => {
+        api_client.loginUser(email, password)
+            .then(res => {
+                assert.equal(res.status, 'OK', 'results should be OK')
+                assert(res.data.token, 'should response data have token')
+                done()
+            })
+            .catch(done)
+    })
+
+    it('retrieve a company NAME by User id', done => {
+        api_client.getCompanyName(userID)
+            .then(res => {
+                expect(res).to.be.equal('DemoCompany02')
+                done()
+            })
+            .catch(done)
+    })
+
+    it('retrieve a company ID by User id', done => {
+        api_client.getCompanyIdByUser(userID)
+            .then(res => {
+                expect(res).to.be.equal('5ab987cc54f2aaf2e220ebd5')
+                done()
+            })
+            .catch(done)
+    })
+
+    it('retrieve events by a company id', done => {
+        api_client.getEventListByCompanyId(companyId)
+            .then(res => {
+                expect(res).to.have.length.above(0)
+                expect(res).to.be.an('array')
+                done()
+            })
+            .catch(done)
+    })
+
+    it('retrieve sessions by company id and event id', done => {
+        api_client.getSessionsList(eventId)
+            .then(res => {
+                expect(res).to.have.length.above(0)
+                expect(res[0].company).to.be.equal('5ab13447031c9c2fa3193b97')
+                expect(res[0].sessions).to.have.length.above(0)
+                expect(res[0].sessions).to.be.an('array')
+                expect(res[0].sessions[0]).to.have.all.keys('_id', 'date', 'location');
+                done()
+            })
+            .catch(done)
+    })
+
+    it('retrieve ticket selected from eventId, sessionId, ticketId', done => {
+        api_client.getTicket(eventId, sessionId, ticketId)
+            .then(res => {
+                assert.equal(res.status, 'OK', 'results should be OK')
+                expect(res.data._id).to.be.equal('5ab13447031c9c2fa3193b9b')
+                done()
+            })
+            .catch(done)
+    })
+
+    it('will create an user inside the company', done => {
+        api_client.createUser(nameUser, usernameUser, emailUser, passwordUser, rolUser, companysId, eventsId)
+        .then (res => {
+            assert.equal(res.status, 'OK', 'results should be OK')
+            expect(res.data).to.be.equal(nameUser)
+            done()
+        })
+        .catch(done)
+    })
+
+    it('will delete an Validator user', done => {
+        api_client.deleteUser(emailUser, rolAdmin, email)
+        .then (res => {
+            assert.equal(res.status, 'OK', 'results should be OK')
+            done()
+        })
+        .catch(done)
+    })
+    
+
+})
